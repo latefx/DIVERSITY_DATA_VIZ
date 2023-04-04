dfa81336757c8f5fbf4a3531448ac63772d1179e
latefx <104640452+latefx@users.noreply.github.com>
2023-04-04 15:54:13 -0400

    Update DIVERSITY_DATA_VIZ.js

--- DIVERSITY_DATA_VIZ.js
+++ DIVERSITY_DATA_VIZ.js
@@ -1,157 +1,93 @@
-document.getElementById("add-store").addEventListener("click", () => addStore());
-document.getElementById("load-data").addEventListener("click", loadData);
-document.getElementById("clear-data").addEventListener("click", clearData);
+const mainContainer = document.getElementById("main-container");
+const storeContainer = document.getElementById("store-container");
+const addStoreBtn = document.getElementById("add-store");
+const chartCanvas = document.getElementById("chart");
 
-const subcategories = ["Skin Tone", "Sizing", "Mannequin Diversity", "Adaptability", "Marketing", "Co-Gender"];
+const subCategories = [
+  "Skin Tone",
+  "Sizing",
+  "Mannequin Diversity",
+  "Adaptability",
+  "Marketing",
+  "Co-Gender",
+];
 
-function addStore(storeName) {
-  if (!storeName) {
-    storeName = document.getElementById("store-name").value;
-    if (!storeName) return;
-    document.getElementById("store-name").value = "";
-  }
+const chartData = {
+  labels: [],
+  datasets: subCategories.map((category) => ({
+    label: category,
+    data: [],
+    backgroundColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
+      Math.random() * 255
+    }, 0.2)`,
+  })),
+};
 
-  const storeContainer = document.createElement("div");
-  storeContainer.classList.add("store");
-
-  const storeTitle = document.createElement("h2");
-  storeTitle.innerHTML = storeName;
-
-  storeContainer.appendChild(storeTitle);
-
-  subcategories.forEach((subcategory) => {
-    const subcategoryDiv = document.createElement("div");
-    subcategoryDiv.classList.add("subcategory");
-
-    const subcategoryName = document.createElement("p");
-    subcategoryName.innerHTML = subcategory;
-
-    const increaseButton = document.createElement("button");
-    increaseButton.innerHTML = "+";
-    increaseButton.addEventListener("click", () => {
-      updateCounter(subcategoryDiv, 1);
-    });
-
-    const decreaseButton = document.createElement("button");
-    decreaseButton.innerHTML = "-";
-    decreaseButton.addEventListener("click", () => {
-      updateCounter(subcategoryDiv, -1);
-    });
-
-    const counter = document.createElement("p");
-    counter.innerHTML = "0";
-
-    subcategoryDiv.appendChild(subcategoryName);
-    subcategoryDiv.appendChild(increaseButton);
-    subcategoryDiv.appendChild(decreaseButton);
-    subcategoryDiv.appendChild(counter);
-
-    storeContainer.appendChild(subcategoryDiv);
-  });
-
-  document.getElementById("store-container").appendChild(storeContainer);
-  updateChart(); // Update the chart after adding a new store
-}
-
-function updateCounter(element, value) {
-  const counter = element.querySelector("p:last-of-type");
-  let count = parseInt(counter.innerHTML);
-  count += value;
-  if (count < 0) count = 0;
-  counter.innerHTML = count;
-  updateChart(); // Update the chart after updating the counter
-}
-
-function loadData() {
-  let data = localStorage.getItem("dataVisualizerData");
-
-  if (!data) return;
-
-  data = JSON.parse(data);
-  document.getElementById("store-container").innerHTML = "";
-  data.forEach((store) => {
-    addStore(store.name);
-    store.tallies.forEach((tally, index) => {
-      document.querySelectorAll(".subcategory")[index].querySelector("p:last-of-type").innerHTML = tally;
-    });
-  });
-
-  updateChart();
-}
-
-let chart;
-
-function updateChart() {
-  if (chart) {
-    chart.destroy();
-  }
-
-  const ctx = document.getElementById("chart").getContext("2d");
-
-  const stores = document.querySelectorAll(".store");
-  const datasets = [];
-
-  subcategories.forEach((subcategory, index) => {
-    const dataset = {
-      label: subcategory,
-      data: [],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ][index % 6],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ][index % 6],
-      borderWidth: 1,
-    };
-
-    stores.forEach((store) => {
-      const tallies = Array.from(store.querySelectorAll(".subcategory p:last-of-type")).map((p) => parseInt(p.innerHTML));
-      dataset.data.push(tallies[index]);
-    });
-
-    datasets.push(dataset);
-  });
-
-  chart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: Array.from(document.querySelectorAll(".store h2")).map((el) => el.innerHTML),
-      datasets: datasets,
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
+const chart = new Chart(chartCanvas.getContext("2d"), {
+  type: "bar",
+  data: chartData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
       },
     },
-  });
-}
+  },
+});
 
-function saveData() {
-  const data = Array.from(document.querySelectorAll(".store")).map((store) => {
-    const tallies = Array.from(store.querySelectorAll(".subcategory p:last-of-type")).map((p) => parseInt(p.innerHTML));
-    return { name: store.querySelector("h2").innerHTML, tallies };
-  });
-
-  localStorage.setItem("dataVisualizerData", JSON.stringify(data));
-}
-
-function clearData() {
-  if (confirm("Are you sure you want to clear all data?")) {
-    localStorage.removeItem("dataVisualizerData");
-    document.getElementById("store-container").innerHTML = "";
+addStoreBtn.addEventListener("click", () => {
+  const storeName = prompt("Enter store name:");
+  if (storeName) {
+    addStore(storeName);
   }
-}
+});
 
-window.addEventListener("beforeunload", saveData);
+function addStore(name) {
+  const storeDiv = document.createElement("div");
+  storeDiv.classList.add("store");
+  storeDiv.innerHTML = `<h3>${name}</h3>`;
+  storeContainer.appendChild(storeDiv);
+
+  chartData.labels.push(name);
+
+  subCategories.forEach((category, index) => {
+    const subCategoryDiv = document.createElement("div");
+    subCategoryDiv.classList.add("subcategory");
+    storeDiv.appendChild(subCategoryDiv);
+
+    const count = document.createElement("span");
+    count.textContent = "0";
+    subCategoryDiv.appendChild(count);
+
+    const title = document.createElement("span");
+    title.textContent = category;
+    subCategoryDiv.appendChild(title);
+
+    const incrementBtn = document.createElement("button");
+    incrementBtn.textContent = "+";
+    incrementBtn.addEventListener("click", () => {
+      count.textContent = parseInt(count.textContent) + 1;
+      chartData.datasets[index].data[
+        chartData.labels.indexOf(name)
+      ] = parseInt(count.textContent);
+      chart.update();
+    });
+    subCategoryDiv.appendChild(incrementBtn);
+
+    const decrementBtn = document.createElement("button");
+    decrementBtn.textContent = "-";
+    decrementBtn.addEventListener("click", () => {
+      if (parseInt(count.textContent) > 0) {
+        count.textContent = parseInt(count.textContent) - 1;
+        chartData.datasets[index].data[
+          chartData.labels.indexOf(name)
+        ] = parseInt(count.textContent);
+        chart.update();
+      }
+    });
+    subCategoryDiv.appendChild(decrementBtn);
+  });
+
+  chart.data.labels = chartData.labels;
+  chart.update();
+}
