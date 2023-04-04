574b0401226faa5171ec4594deb0c524685b81a0
latefx <104640452+latefx@users.noreply.github.com>
2023-04-04 14:31:52 -0400

    Update DIVERSITY_DATA_VIZ.js

--- DIVERSITY_DATA_VIZ.js
+++ DIVERSITY_DATA_VIZ.js
@@ -59,8 +59,6 @@ function updateCounter(element, value) {
   counter.innerHTML = count;
 }
 
-let chart; // Added this line outside the loadData function.
-
 function loadData() {
   let data = localStorage.getItem("dataVisualizerData");
 
@@ -76,6 +74,66 @@ function loadData() {
   });
 
   updateChart(); // Add this line at the end of loadData function.
+}
+
+let chart; // Added this line outside the loadData function.
+
+function updateChart() {
+  if (chart) {
+    chart.destroy();
+  }
+
+  const ctx = document.getElementById("chart").getContext("2d");
+
+  // Prepare datasets
+  const stores = document.querySelectorAll(".store");
+  const datasets = [];
+
+  subcategories.forEach((subcategory, index) => {
+    const dataset = {
+      label: subcategory,
+      data: [],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ][index % 6],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ][index % 6],
+      borderWidth: 1,
+    };
+
+    stores.forEach((store) => {
+      const tallies = Array.from(store.querySelectorAll(".subcategory p:last-of-type")).map((p) => parseInt(p.innerHTML));
+      dataset.data.push(tallies[index]);
+    });
+
+    datasets.push(dataset);
+  });
+
+  chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: Array.from(document.querySelectorAll(".store h2")).map((el) => el.innerHTML),
+      datasets: datasets,
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
 }
 
 function saveData() {
