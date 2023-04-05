8271a8d92e858c27eff7bacf1767356ee30d2f05
latefx <104640452+latefx@users.noreply.github.com>
2023-04-04 22:35:10 -0400

    Update comparison.js

--- comparison.js
+++ comparison.js
@@ -18,15 +18,27 @@ if (storeNames.length) {
     }
   });
 
+  const yesData = storesData.map(store => store.data.reduce((count, val) => (val === 1 ? count + 1 : count), 0));
+  const noData = storesData.map(store => store.data.reduce((count, val) => (val === 0 ? count + 1 : count), 0));
+
   const chartData = {
     labels: storesData.map(store => store.storeName),
-    datasets: savedData.subCategories ? savedData.subCategories.map((category, index) => ({
-      label: category,
-      data: storesData.map(store => store.data[index]),
-      backgroundColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
-        Math.random() * 255
-      }, 0.2)`,
-    })) : []
+    datasets: [
+      {
+        label: "Yes",
+        data: yesData,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1
+      },
+      {
+        label: "No",
+        data: noData,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1
+      }
+    ]
   };
 
   const chart = new Chart(chartCanvas.getContext("2d"), {
@@ -38,6 +50,15 @@ if (storeNames.length) {
           beginAtZero: true,
         },
       },
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Store Comparison"
+        }
+      }
     },
   });
 }
